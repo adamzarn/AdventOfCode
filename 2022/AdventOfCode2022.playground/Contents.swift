536c2c5847b17e334a617df233850a822dda6610
Adam Zarn <ajzarn@gmail.com>
2022-12-01 11:08:39 -0600

    Add faster Day1 alternative

--- 2022/AdventOfCode2022.playground/Contents.swift
+++ 2022/AdventOfCode2022.playground/Contents.swift
@@ -1,1 +1,12 @@
-Day1.run()
+import Foundation
+
+printTimeElapsedWhenRunningCode(title: "Day1") {
+    Day1.runFaster()
+}
+
+func printTimeElapsedWhenRunningCode(title:String, operation:()->()) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    operation()
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print("Time elapsed for \(title): \(timeElapsed) s.")
+}
--- 2022/AdventOfCode2022.playground/Sources/Day1.swift
+++ 2022/AdventOfCode2022.playground/Sources/Day1.swift
@@ -15,6 +15,30 @@ public class Day1: Day {
         print("The three elves with the most calories have a total of \(totalCaloriesOfThreeElvesWithMostCalories) calories.")
     }
     
+    public static func runFaster() {
+        guard let string = getInputString() else { exit(0) }
+        
+        // Part One
+        let totalCaloriesOfEachElf = getTotalCaloriesOfEachElf(string)
+        print("The elf with the most calories has \(totalCaloriesOfEachElf.max() ?? 0) calories.")
+        
+        // Part Two
+        var max1 = 0
+        var max2 = 0
+        var max3 = 0
+        for calories in totalCaloriesOfEachElf {
+            if calories > max1 {
+                max1 = calories
+            } else if calories > max2 {
+                max2 = calories
+            } else if calories > max3 {
+                max3 = calories
+            }
+        }
+        let totalCaloriesOfThreeElvesWithMostCalories = max1 + max2 + max3
+        print("The three elves with the most calories have a total of \(totalCaloriesOfThreeElvesWithMostCalories) calories.")
+    }
+    
     static func getTotalCaloriesOfEachElf(_ string: String) -> [Int] {
         let strings = string.components(separatedBy: "\n\n")
         return strings.map { string in
