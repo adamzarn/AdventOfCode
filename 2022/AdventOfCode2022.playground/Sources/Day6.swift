995ff4d44257e2f611d5ca7db81cfe1b61413ebe
Adam Zarn <ajzarn@gmail.com>
2022-12-06 22:03:40 -0600

    Clean up

--- 2022/AdventOfCode2022.playground/Sources/Day6.swift
+++ 2022/AdventOfCode2022.playground/Sources/Day6.swift
@@ -2,12 +2,11 @@ import Foundation
 
 public class Day6: Day {
     public static func run() {
-        let partOne: Bool = false
-        let length: Int = partOne ? 4 : 14
         guard let string = getInputString() else { exit(0) }
+        let partOne: Bool = true
+        let length: Int = partOne ? 4 : 14
         let characters = string.map { String($0) }
-        for index in 0..<characters.count {
-            guard index > length - 1 else { continue }
+        for index in (length-1)..<characters.count {
             if Set(characters[(index-(length-1))...index]).count == length {
                 print(index + 1)
                 return
